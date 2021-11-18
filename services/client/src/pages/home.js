dbc70a4026c58cca17c61daa19bb1cd33bcbff56
Sean <sean.liao@ailabpte.co>
2021-11-18 19:01:36 +0800

    update css and home page

--- services/client/src/pages/home.js
+++ services/client/src/pages/home.js
@@ -1,29 +1,29 @@
-import React from 'react'
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
-import { Link } from 'react-router-dom'
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-    return (
-      <div className="home-bg">
-        <div className="center">
-          <h1 className="title">Start your journy here</h1>
-          <Card className="text-center">
-            <Card.Body>
-              <Button variant="light">
-                <Link to="/register">Register</Link>
-              </Button>
-              <Button variant="light">
-                <Link to="/login">Login</Link>
-              </Button>
-              <Button variant="light">
-                <Link to="/search">Visit</Link>
-              </Button>
-            </Card.Body>
-          </Card>
-        </div>
-      </div>
-    );
-}
+	return (
+		<div className="home-bg">
+			<div className="center">
+				<h1 className="title">Find your learning partner here</h1>
+				<Card className="text-center opacity">
+					<Card.Body>
+						<Button variant="outline-secondary " className="m-2">
+							<Link to="/register">Register</Link>
+						</Button>
+						<Button variant="outline-secondary " className="m-2">
+							<Link to="/login">Login</Link>
+						</Button>
+						<Button variant="outline-secondary " className="m-2">
+							<Link to="/search">Visit</Link>
+						</Button>
+					</Card.Body>
+				</Card>
+			</div>
+		</div>
+	);
+};
 
-export default Home
+export default Home;
--- services/client/src/pages/search.js
+++ services/client/src/pages/search.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from "react";
-import { PopBoxCxt } from '../component/contexts'
+import { PopBoxCxt } from "../component/contexts";
 import Form from "react-bootstrap/Form";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
@@ -8,17 +8,16 @@ import { matchUserByLang } from "../api";
 import LangGroup from "../component/langGroup";
 
 const Search = () => {
-    const { setPopbox } = useContext(PopBoxCxt);
-    
-    const { allLang, selectLevelHandler, selectLangHandler, selLang } =
+	const { setPopbox } = useContext(PopBoxCxt);
+
+	const { allLang, selectLevelHandler, selectLangHandler, selLang } =
 		useSelLang();
 
-    const submit = async () => {
+	const submit = async () => {
+		try {
+			const response = await matchUserByLang(selLang);
 
-      try {
-        const response = await matchUserByLang(selLang);
-        
-        /*
+			/*
         if (response.data.errorCode === 2) {
           setStatus(prev => ({
             ...prev,
@@ -29,36 +28,35 @@ const Search = () => {
           }));
         } 
         */
-      } catch (error) {
-        console.error(error);
-        setPopbox({
-          isShow: true,
-          content: "Server error",
-        });
-      }
-      
-  }
-  
+		} catch (error) {
+			console.error(error);
+			setPopbox({
+				isShow: true,
+				content: "Server error"
+			});
+		}
+	};
 
-    return (
-      <div className="bg2">
-        <Card className="text-center center">
-          <Card.Body>
-            <Form>
-              <LangGroup
-                allLang={allLang}
-                selectLevelHandler={selectLevelHandler}
-                selectLangHandler={selectLangHandler}
-                form={selLang}
-              />
-            </Form>
-            <Button onClick={submit} variant="light" type="button">
-              Find Partners
-            </Button>
-          </Card.Body>
-        </Card>
-      </div>
-    );
-}
+	return (
+		<div className="bg-search">
+			<Card className="text-center center">
+				<Card.Header>Search</Card.Header>
+				<Card.Body>
+					<Form>
+						<LangGroup
+							allLang={allLang}
+							selectLevelHandler={selectLevelHandler}
+							selectLangHandler={selectLangHandler}
+							form={selLang}
+						/>
+					</Form>
+					<Button onClick={submit} variant="light" type="button">
+						Find partners
+					</Button>
+				</Card.Body>
+			</Card>
+		</div>
+	);
+};
 
-export default Search
+export default Search;
